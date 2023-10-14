7d13cef8bee3c2b0d87236902d1c1329cf057e49
XXXDESHXXX <kirya.shvarts@list.ru>
2023-10-14 22:56:51 +0300

    Use registration logic

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from database import create_tables
+from services.io import get_password, get_username, is_want_to_register
+from services.auth import register
 
 
 def main():
     create_tables()
+    if is_want_to_register():
+        password = get_password()
+        username = get_username()
+        register(username, password)
 
 
 if __name__ == "__main__":
