4db55cdffe9de5dc6fc93d3f098ac9dd8e2f609e
XXXDESHXXX <kirya.shvarts@list.ru>
2023-10-14 22:55:25 +0300

    Replace get_password and get_username raise keyword with print function

--- services/io.py
+++ services/io.py
@@ -1,22 +1,37 @@
 from constants import PROHIBITED_PASSWORD_CHARACTERS, MAX_PASSWORD_LENGTH, MAX_USERNAME_LENGTH
 
 
+def is_want_to_register():
+    answer = input("Choose the option:\n1. Register\n2. Log in\n")
+    if answer != "1" and answer != "2":
+        print(f"Invalid option selected")
+        return is_want_to_register()
+    match answer:
+        case "1":
+            return True
+        case "2":
+            return False
+
+
 def get_password():
     joined_prohibited_password_characters = ", ".join(PROHIBITED_PASSWORD_CHARACTERS)
     password = input("Enter a password: ")
-    if len(password) <= MAX_PASSWORD_LENGTH:
-        raise ValueError(f"Password length must be more than {MAX_PASSWORD_LENGTH} symbols or equal")
+    if len(password) < MAX_PASSWORD_LENGTH:
+        print(f"Password length must be more than {MAX_PASSWORD_LENGTH} symbols or equal")
+        return get_password()
     for character in PROHIBITED_PASSWORD_CHARACTERS:
         if character in password:
-            raise ValueError(
+            print(
                 f"{character} is not allowed in password,"
                 f" there are such prohibited characters: {joined_prohibited_password_characters}"
             )
+            return get_password()
     return password
 
 
 def get_username():
     username = input("Enter a username: ")
-    if len(username) <= MAX_USERNAME_LENGTH:
-        raise ValueError(f"Username length must be more than {MAX_USERNAME_LENGTH} symbols or equal")
+    if len(username) < MAX_USERNAME_LENGTH:
+        print(f"Username length must be more than {MAX_USERNAME_LENGTH} symbols or equal")
+        return get_username()
     return username
