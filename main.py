aa5a6858ac45c58165f4bb8d41c03cb0506a12d4
XXXDESHXXX <kirya.shvarts@list.ru>
2023-10-15 21:53:49 +0300

    Add login logic: refactor

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from database import create_tables
 from services.io import get_password, get_username, get_auth_option, AuthOptions
-from services.auth import register
+from services.auth import register, log_in
 
 
 def main():
     create_tables()
-    match get_auth_option():
-        case AuthOptions.REGISTER:
-            password = get_password()
-            username = get_username()
-            register(username, password)
-        case AuthOptions.LOG_IN:
-            pass
+    auth_option = get_auth_option()
+    username = get_username()
+    password = get_password()
+    if auth_option == AuthOptions.REGISTER:
+        register(username, password)
+    user = log_in(username, password)
+    while user is None:
+        print("Invalid username")
+        user = log_in(get_username(), get_password())
 
 
 if __name__ == "__main__":
