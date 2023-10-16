0659705ee77869086e93681a994174100a6b8b80
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-10-16 15:54:22 +0300

    Simplification | Readability

--- selectors.py
+++ selectors.py
@@ -1,12 +1,11 @@
 from database import cur
 
 
-def get_user(username: str) -> tuple[int, str, str]:
+def get_user(username: str) -> tuple[int, str, str] | None:
     cur.execute(
         f"""
         SELECT * FROM auth_user 
         WHERE username = '{username}'
         """
     )
-    user = cur.fetchone()
-    return user
+    return cur.fetchone()
